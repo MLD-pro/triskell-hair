4f84f5e78c9df7f5a231b8f064ccbb9e4a2f6824
MLD-pro <morgan.ld.pro@gmail.com>
2025-10-02 17:11:14 +0200

    modification du formulaire de RDV Appointement

--- src/Form/AppointementType.php
+++ src/Form/AppointementType.php
@@ -7,6 +7,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TelType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -18,13 +19,13 @@ class AppointementType extends AbstractType
     {
         $builder
             ->add('civility', ChoiceType::class, [
-                'choices' => [
-                    'Madame' => 'Madame',
-                    'Monsieur' => 'Monsieur',
-                ],
-                'expanded' => true, // radio boutons
-                'multiple' => false,
                 'label' => 'Civilité',
+                'choices' => [
+                    'Monsieur' => 'M.',
+                    'Madame' => 'Mme',
+                ],
+                'expanded' => true,
+                'multiple' => false,
             ])
             ->add('lastName', TextType::class, [
                 'label' => 'Nom',
@@ -38,75 +39,73 @@ class AppointementType extends AbstractType
                 'label' => 'Email',
                 'attr' => ['placeholder' => 'Votre adresse email'],
             ])
-            ->add('phone', TextType::class, [
+            ->add('phone', TelType::class, [
                 'label' => 'Téléphone',
                 'attr' => ['placeholder' => 'Votre numéro de téléphone'],
             ])
             ->add('service', TextType::class, [
                 'label' => 'Prestation souhaitée',
-                'attr' => ['placeholder' => 'Ex: Coupe, coloration…'],
+                'attr' => ['placeholder' => 'Ex : Coupe, couleur...'],
             ])
             ->add('date1', DateType::class, [
-                'label' => '1ère date souhaitée',
+                'label' => 'Date souhaitée (1er choix)',
                 'widget' => 'single_text',
-                'required' => false,
             ])
             ->add('moment1', ChoiceType::class, [
+                'label' => 'Créneau (1er choix)',
                 'choices' => [
-                    'Matin' => 'Matin',
-                    'Après-midi' => 'Après-midi',
+                    'Matin' => 'matin',
+                    'Après-midi' => 'apresmidi',
                 ],
-                'expanded' => true,
-                'multiple' => false,
-                'label' => 'Créneau 1',
-                'required' => false,
+                'expanded' => true, // radios
+                'multiple' => true,
             ])
             ->add('date2', DateType::class, [
-                'label' => '2ème date souhaitée',
+                'label' => 'Date souhaitée (2e choix)',
                 'widget' => 'single_text',
                 'required' => false,
             ])
             ->add('moment2', ChoiceType::class, [
+                'label' => 'Créneau (2e choix)',
                 'choices' => [
-                    'Matin' => 'Matin',
-                    'Après-midi' => 'Après-midi',
+                    'Matin' => 'matin',
+                    'Après-midi' => 'apresmidi',
                 ],
                 'expanded' => true,
-                'multiple' => false,
-                'label' => 'Créneau 2',
+                'multiple' => true,
                 'required' => false,
             ])
             ->add('date3', DateType::class, [
-                'label' => '3ème date souhaitée',
+                'label' => 'Date souhaitée (3e choix)',
                 'widget' => 'single_text',
                 'required' => false,
             ])
             ->add('moment3', ChoiceType::class, [
+                'label' => 'Créneau (3e choix)',
                 'choices' => [
-                    'Matin' => 'Matin',
-                    'Après-midi' => 'Après-midi',
+                    'Matin' => 'matin',
+                    'Après-midi' => 'apresmidi',
                 ],
                 'expanded' => true,
-                'multiple' => false,
-                'label' => 'Créneau 3',
+                'multiple' => true,
                 'required' => false,
             ])
             ->add('address', TextType::class, [
                 'label' => 'Adresse',
-                'attr' => ['placeholder' => 'Votre adresse complète'],
+                'attr' => ['placeholder' => 'Votre adresse'],
             ])
             ->add('zipcode', TextType::class, [
                 'label' => 'Code postal',
                 'attr' => ['placeholder' => 'Votre code postal'],
             ])
             ->add('city', TextType::class, [
-                'label' => 'Ville / Commune',
+                'label' => 'Ville',
                 'attr' => ['placeholder' => 'Votre ville'],
             ])
             ->add('message', TextareaType::class, [
-                'label' => 'Message (facultatif)',
+                'label' => 'Message complémentaire (facultatif)',
                 'required' => false,
-                'attr' => ['placeholder' => 'Informations complémentaires'],
+                'attr' => ['placeholder' => 'Votre message...'],
             ])
         ;
     }
@@ -118,4 +117,5 @@ class AppointementType extends AbstractType
         ]);
     }
 }
+
 

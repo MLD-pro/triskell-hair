7148ef67081b119fe5855508eb3be709331bd241
MLD-pro <morgan.ld.pro@gmail.com>
2025-10-08 15:51:52 +0200

    base twig pour le header et footer en cours

---
+++ public/js/main.js
@@ -0,0 +1,9 @@
+
+document.addEventListener("DOMContentLoaded", () => {
+    const burger = document.getElementById("burger-btn");
+    const menu = document.getElementById("nav-menu");
+
+    burger.addEventListener("click", () => {
+        menu.classList.toggle("open");
+    });
+});

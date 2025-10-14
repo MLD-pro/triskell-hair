aaa951528e136166c57d7cf92b3bb4aab8ba26b0
MLD-pro <morgan.ld.pro@gmail.com>
2025-10-14 17:48:46 +0200

    mise ne place de phpmailer avec envoie de mail auto lors d'une demande de rdv

--- src/Controller/AppointementController.php
+++ src/Controller/AppointementController.php
@@ -9,6 +9,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
 
 class AppointementController extends AbstractController
 {
@@ -17,7 +19,7 @@ class AppointementController extends AbstractController
     {
         $appointement = new Appointement();
 
-        // 🔹 Si l'utilisateur est connecté, préremplir les infos connues
+        // Si l'utilisateur est connecté, préremplir les infos connues
         $user = $this->getUser();
         if ($user) {
             $appointement->setLastname($user->getLastName());
@@ -38,6 +40,51 @@ class AppointementController extends AbstractController
             $em->persist($appointement);
             $em->flush();
 
+            //  Envoi du mail automatique avec PHPMailer
+            try {
+                $mail = new PHPMailer(true);
+                $mail->SMTPDebug = 2; // 🔍 affiche les détails de la connexion SMTP
+                $mail->Debugoutput = 'error_log'; // 🔍 envoie les logs vers error_log()
+
+                $mail->isSMTP();
+                $mail->Host = 'smtp.gmail.com';
+                $mail->SMTPAuth = true;
+                $mail->Username = $_ENV['MAIL_USER'];
+                $mail->Password = $_ENV['MAIL_PASS'];
+                $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+                $mail->Port = 587;
+
+                // Désactive la vérification SSL (utile en local)
+                $mail->SMTPOptions = [
+                    'ssl' => [
+                        'verify_peer' => false,
+                        'verify_peer_name' => false,
+                        'allow_self_signed' => true,
+                    ],
+                ];
+
+                // Expéditeur et destinataire
+                $mail->setFrom($_ENV['MAIL_USER'], 'Triskell Hair');
+                $mail->addAddress(
+                    $appointement->getEmail(),
+                    $appointement->getFirstName() . ' ' . $appointement->getLastName()
+                );
+
+                // Contenu du mail
+                $mail->isHTML(true);
+                $mail->Subject = 'Votre demande de rendez-vous chez Triskell Hair';
+                $mail->Body = "
+                    <p>Bonjour <strong>{$appointement->getFirstName()}</strong>,</p>
+                    <p>J'ai bien reçu votre demande de rendez-vous.</p>
+                    <p>Je vous contacte par téléphone très prochainement afin de convenir ensemble d’un horaire précis.</p>
+                    <p>Merci pour votre confiance et à bientôt,<br><strong>Claire de Triskell Hair</strong></p>
+                ";
+
+                $mail->send();
+            } catch (Exception $e) {
+                dd('Erreur PHPMailer : ' . $mail->ErrorInfo);
+            }
+
             // Message de confirmation
             $this->addFlash('success', 'Votre demande de rendez-vous a bien été envoyée. Vous serez contacté rapidement.');
 
@@ -50,5 +97,9 @@ class AppointementController extends AbstractController
         ]);
     }
 }
+
+
+
+
 
 

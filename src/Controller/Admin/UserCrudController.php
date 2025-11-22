6dc7bc51df76d09be8847287f6d589bf783e37ba
MLD-pro <morgan.ld.pro@gmail.com>
2025-11-22 08:46:11 +0100

    affichage admin et utilisateur sur l'interface EasyAdmin

--- src/Controller/Admin/UserCrudController.php
+++ src/Controller/Admin/UserCrudController.php
@@ -5,8 +5,10 @@ namespace App\Controller\Admin;
 use App\Entity\User;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\EmailField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TelephoneField;
 
 class UserCrudController extends AbstractCrudController
 {
@@ -15,14 +17,33 @@ class UserCrudController extends AbstractCrudController
         return User::class;
     }
 
-    /*
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            IdField::new('id')->onlyOnIndex(),
+
+            EmailField::new('email', 'Email'),
+
+            TextField::new('lastName', 'Nom'),
+            TextField::new('firstName', 'Prénom'),
+            TextField::new('city', 'Ville')->hideOnIndex(),
+            TextField::new('zipcode', 'Code postal')->hideOnIndex(),
+            TextField::new('address', 'Adresse')->hideOnIndex(),
+            TelephoneField::new('phone', 'Téléphone')->hideOnIndex(),
+
+            // Choix des rôles (modifiable depuis l’admin)
+            ChoiceField::new('roles', 'Rôles')
+                ->allowMultipleChoices()
+                ->setChoices([
+                    'Utilisateur' => 'ROLE_USER',
+                    'Administrateur' => 'ROLE_ADMIN',
+                ])
+                ->renderExpanded(false) // select normal
+                ->renderAsBadges()      // joli affichage dans la liste
         ];
     }
-    */
 }
+
+
+
+
